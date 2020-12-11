f339c5879e094c35f9edc892384cb96af1443df4
vamsirajan <59833105+vamsirajan@users.noreply.github.com>
2020-12-11 13:29:54 +0530

    Initial Phase

---
+++ main.py
@@ -0,0 +1,13 @@
+import time  #time module
+from plyer import notification  #A module which helps us in bringing up the notifications
+
+if  __name__ == "__main__":
+    while(True):
+        notification.notify(
+            title = "Please Drink Water!!!",
+            message = "A Glass of water every hour is MUST!!!",
+            app_icon = "C:/Users/vamsi rajan/pythonpro/project1/glass.ico",  
+            #Cannot directly place C:\Users\vamsi rajan\pythonpro\project1
+            timeout = 10
+        )
+        time.sleep(60*60)  #Notifies us after every hour
